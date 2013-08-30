4d4185f9d2bede963d63d18d8ddb19e231b26054
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2013-08-30 23:36:53 +0200

    Fix pawn movement

--- src/name/matco/rookoid/game/piece/Pawn.java
+++ src/name/matco/rookoid/game/piece/Pawn.java
@@ -7,10 +7,11 @@ import name.matco.rookoid.R;
 import name.matco.rookoid.game.Movement;
 import name.matco.rookoid.game.Movement.Direction;
 import name.matco.rookoid.game.Player;
+import name.matco.rookoid.game.exception.OutOfBoardCoordinateException;
 
 public class Pawn extends Piece {
 	
-	public Pawn(Player player) {
+	public Pawn(final Player player) {
 		super(player);
 	}
 	
@@ -26,22 +27,58 @@ public class Pawn extends Piece {
 	
 	@Override
 	public List<List<Movement>> getAllowedMovements() {
-		List<Movement> movements = new ArrayList<Movement>();
+		final List<Movement> movements = new ArrayList<Movement>();
 		if (Player.WHITE.equals(getPlayer())) {
-			movements.add(Direction.SOUTH.getMovement());
-			// not been played yet
-			if (square.getCoordinate().y == 1) {
-				movements.add(new Movement(0, 2)); // TODO : use SOUTH?
+			try {
+				// pawn can not capture on line movements
+				if (getSquare().apply(Direction.SOUTH.getMovement()).getPiece() == null) {
+					movements.add(Direction.SOUTH.getMovement());
+					// not been played yet
+					if (square.getCoordinate().y == 1) {
+						final Movement twoSquares = new Movement(0, 2);
+						if (getSquare().apply(twoSquares).getPiece() == null) {
+							movements.add(twoSquares); // TODO : use SOUTH?
+						}
+					}
+				}
+				// pawn can move on diagonal if there is a piece to capture
+				if (getSquare().apply(Direction.SOUTH_EAST.getMovement()).getPiece() != null) {
+					movements.add(Direction.SOUTH_EAST.getMovement());
+				}
+				// pawn can move on diagonal if there is a piece to capture
+				if (getSquare().apply(Direction.SOUTH_WEST.getMovement()).getPiece() != null) {
+					movements.add(Direction.SOUTH_WEST.getMovement());
+				}
+			} catch (final OutOfBoardCoordinateException e) {
+				// out of board moves not allowed
 			}
 		}
 		else {
-			movements.add(Direction.NORTH.getMovement());
-			// not been played yet
-			if (square.getCoordinate().y == 6) {
-				movements.add(new Movement(0, -2)); // TODO : use NORTH?
+			try {
+				// pawn can not capture on line movements
+				if (getSquare().apply(Direction.NORTH.getMovement()).getPiece() == null) {
+					movements.add(Direction.NORTH.getMovement());
+					// not been played yet
+					if (square.getCoordinate().y == 6) {
+						final Movement twoSquares = new Movement(0, -2);
+						if (getSquare().apply(twoSquares).getPiece() == null) {
+							movements.add(twoSquares); // TODO : use NORTH?
+						}
+					}
+				}
+				// pawn can move on diagonal if there is a piece to capture
+				if (getSquare().apply(Direction.NORTH_EAST.getMovement()).getPiece() != null) {
+					movements.add(Direction.NORTH_EAST.getMovement());
+				}
+				// pawn can move on diagonal if there is a piece to capture
+				if (getSquare().apply(Direction.NORTH_WEST.getMovement()).getPiece() != null) {
+					movements.add(Direction.NORTH_WEST.getMovement());
+				}
+			} catch (final OutOfBoardCoordinateException e) {
+				// out of board moves not allowed
 			}
 		}
-		List<List<Movement>> ret = new ArrayList<List<Movement>>();
+		final List<List<Movement>> ret = new ArrayList<List<Movement>>();
 		ret.add(movements);
 		return ret;
 	}
